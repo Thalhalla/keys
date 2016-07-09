c10c83e1a2dd899bacc7adc43cbb67665fd6c0b4
Josh Cox <joshuacox@users.noreply.github.com>
2016-07-09 09:31:46 -0500

    speedracer
    this script has proven itself, I am removing the holds as now I'm provisioning many servers at once using this method and I don't care to multiply 5 seconds times 100, use at your own risk!!!!

--- addus.sh
+++ addus.sh
@@ -8,7 +8,7 @@ rm /tmp/rootsshauthorized_keys
 echo 'I will be adding these entries (removing duplicates):'
 diff /tmp/rootsshauthorized_keysuniq /root/.ssh/authorized_keys
 echo -n 'ctrl-C to stop now! or forever hold your peace'
-sleep 1;echo -n '.';sleep 1;echo -n '.';sleep 1;echo -n '.';sleep 1;echo -n '.'
-sleep 1
+#sleep 1;echo -n '.';sleep 1;echo -n '.';sleep 1;echo -n '.';sleep 1;echo -n '.'
+#sleep 1
 cp /tmp/rootsshauthorized_keysuniq /root/.ssh/authorized_keys
 rm /tmp/rootsshauthorized_keysuniq
