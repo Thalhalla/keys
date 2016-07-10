7c3fd2dbc0feaec8c08f1417abfc4fb14662bc98
Josh Cox <josh@webhosting.coop>
2016-07-10 15:48:10 -0500

    curlfree

--- addus.sh
+++ addus.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+TMP=mktemp -d
+cd $TMP
 echo 'This is for Thalhalla dev only, this will add our keys to your server, BEWARE!'
 touch /root/.ssh/authorized_keys
-cp /root/.ssh/authorized_keys /tmp/rootsshauthorized_keys
-curl https://raw.githubusercontent.com/Thalhalla/keys/master/keys |xargs -n1 -I{} echo '{}'>> /tmp/rootsshauthorized_keys
-cat /tmp/rootsshauthorized_keys|sort|uniq>/tmp/rootsshauthorized_keysuniq
-rm /tmp/rootsshauthorized_keys
+cp /root/.ssh/authorized_keys $TMP/rootsshauthorized_keys
+wget --quiet -c https://raw.githubusercontent.com/Thalhalla/keys/master/keys
+cat keys |xargs -n1 -I{} echo '{}'>> $TMP/rootsshauthorized_keys
+cat $TMP/rootsshauthorized_keys|sort|uniq>$TMP/rootsshauthorized_keysuniq
+rm $TMP/rootsshauthorized_keys
 echo 'I will be adding these entries (removing duplicates):'
-diff /tmp/rootsshauthorized_keysuniq /root/.ssh/authorized_keys
+diff $TMP/rootsshauthorized_keysuniq /root/.ssh/authorized_keys
 echo -n 'ctrl-C to stop now! or forever hold your peace'
 #sleep 1;echo -n '.';sleep 1;echo -n '.';sleep 1;echo -n '.';sleep 1;echo -n '.'
 #sleep 1
-cp /tmp/rootsshauthorized_keysuniq /root/.ssh/authorized_keys
-rm /tmp/rootsshauthorized_keysuniq
+cp $TMP/rootsshauthorized_keysuniq /root/.ssh/authorized_keys
+rm $TMP/rootsshauthorized_keysuniq
